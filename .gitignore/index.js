2faeaf52d67e0950bafbc003ccbda350fc6e0c0e
TommyQC <44536691+TommyQC@users.noreply.github.com>
2019-01-06 18:17:18 -0500

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -10,7 +10,7 @@ const queue = new Map();
 
 var servers = {};
 
-client.login("NTI3OTc1MTI4NTI1NzAxMTIw.DxQdkA.CaFKKCePGdXydjLA7NPvnbIZSf4");
+client.login(process.env.TOKEN);
 
 function play(connection, message) {
   
